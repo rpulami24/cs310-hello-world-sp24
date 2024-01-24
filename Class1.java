6dd31fbea33eb11c10fa3b2f3f8983a1ce82faa8
rpulami24 <157573657+rpulami24@users.noreply.github.com>
2024-01-24 13:50:59 -0600

    Started work on f4()

--- Class1.java
+++ Class1.java
@@ -5,8 +5,19 @@
 
 public class Class1 {
 
+    private void f1() {
+        // v3.0
+    }
+
+    private void f2() {
+        // v3.0
+    }
+
     private void f3() {
         // v3.0
+    }
+    private void f4() {
+        // v4.0 (under development)
     }
     
 }
